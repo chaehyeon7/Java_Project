9628c4b66eb875b729fa0a11f2ce263bbe42eba4
jeongnara <79076679+jeongnara@users.noreply.github.com>
2021-12-03 12:15:10 +0900

    사진 커밋 + 버튼 클릭 동작구현

--- app/src/main/java/kr/hs/emirim/chaehyeon/java_project/MainActivity.java
+++ app/src/main/java/kr/hs/emirim/chaehyeon/java_project/MainActivity.java
@@ -8,7 +8,7 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+public class MainActivity extends AppCompatActivity {
     Button button1;
     Button button2;
 
@@ -20,26 +20,36 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         button1 = findViewById(R.id.button1);
         button2 = findViewById(R.id.button2);
 
-        button1=findViewById(R.id.button1);
-        button2=findViewById(R.id.button2);
+        button1 = findViewById(R.id.button1);
+        button2 = findViewById(R.id.button2);
 
-        button1.setOnClickListener(this);
-        button2.setOnClickListener(this);
-    }
-    @Override
-    public void onClick(View v){
-        switch(v.getId()){
-            case R.id.button1:
-                Toast.makeText(MainActivity.this, "버튼 1을 클릭하였습니다.", Toast.LENGTH_LONG).show();
-                break;
-            case R.id.button2:
-                Toast.makeText(MainActivity.this, "버튼 2을 클릭하였습니다.", Toast.LENGTH_LONG).show();
-                break;
-            default:
-                break;
-        }
+//        button1.setOnClickListener(this);
+//        button2.setOnClickListener(this);
+
+        button1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(), activity_selection_screen.class);
+                startActivity(intent);
+            }
+        });
     }
 }
+
+//    @Override
+//    public void onClick(View v){
+//        switch(v.getId()){
+//            case R.id.button1:
+//                Toast.makeText(MainActivity.this, "버튼 1을 클릭하였습니다.", Toast.LENGTH_LONG).show();
+//                break;
+//            case R.id.button2:
+//                Toast.makeText(MainActivity.this, "버튼 2을 클릭하였습니다.", Toast.LENGTH_LONG).show();
+//                break;
+//            default:
+//                break;
+//        }
+//    }
+//}
 
 
 //        button1.setOnClickListener(new View.OnClickListener() {//버튼 이벤트 처리
