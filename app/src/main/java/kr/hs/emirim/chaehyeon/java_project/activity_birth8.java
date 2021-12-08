597339cebeec42767e8223d48642cbd60d26e0c7
chaehyeon8 <81363193+chaehyeon7@users.noreply.github.com>
2021-12-08 13:09:42 +0900

    레이아웃

--- app/src/main/java/kr/hs/emirim/chaehyeon/java_project/activity_birth8.java
+++ app/src/main/java/kr/hs/emirim/chaehyeon/java_project/activity_birth8.java
@@ -4,13 +4,13 @@ import android.os.Bundle;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-public class activity_type2 extends AppCompatActivity {
+public class activity_birth8 extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_type2);
-
+        setContentView(R.layout.activity_birth8);
 
     }
+
 }
--- app/src/main/java/kr/hs/emirim/chaehyeon/java_project/activity_type1.java
+++ app/src/main/java/kr/hs/emirim/chaehyeon/java_project/activity_type1.java
@@ -1,6 +1,8 @@
 package kr.hs.emirim.chaehyeon.java_project;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -10,5 +12,13 @@ public class activity_type1 extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_type1);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override public void run() {
+                Intent intent = new Intent(activity_type1.this, activity_birth1.class);
+                startActivity(intent);
+            }
+            }, 4000);
+
     }
 }
