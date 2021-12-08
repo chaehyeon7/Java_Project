6cc7699be8d7da9720b3dbcb40b1d4c0080a6dbf
chaehyeon8 <81363193+chaehyeon7@users.noreply.github.com>
2021-12-08 14:13:02 +0900

    다음 버튼 넣기

--- app/src/main/java/kr/hs/emirim/chaehyeon/java_project/activity_type1.java
+++ app/src/main/java/kr/hs/emirim/chaehyeon/java_project/activity_type1.java
@@ -3,22 +3,27 @@ package kr.hs.emirim.chaehyeon.java_project;
 import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
+import android.view.View;
+import android.widget.Button;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class activity_type1 extends AppCompatActivity {
+    Button btn1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_type1);
 
-        new Handler().postDelayed(new Runnable() {
-            @Override public void run() {
-                Intent intent = new Intent(activity_type1.this, activity_birth1.class);
-                startActivity(intent);
-            }
-            }, 4000);
+        btn1 = findViewById(R.id.btn1);
 
+        btn1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent btn1 = new Intent(getApplicationContext(), MainActivity.class);
+                startActivity(btn1);
+            }
+        });
     }
 }
